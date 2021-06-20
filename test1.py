7ee5a1e670fc64caae9868c4625b6501daa32794
TamHov <86202638+TamHov@users.noreply.github.com>
2021-06-20 21:56:41 +0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,3 +1,5 @@
+#Login with existing user
+
 from selenium import webdriver
 
 #path of driver should be specified
@@ -8,6 +10,6 @@ driver.get("http://www.facebook.com")
 username = driver.find_element_by_name("email")
 password = driver.find_element_by_name("pass")
 submit = driver.find_element_by_name("login")
-username.send_keys("d@mail.ru")
+username.send_keys("test@yopmail.com")
 password.send_keys("aaaa")
 submit.click()
